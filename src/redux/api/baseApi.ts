a26920c0601063be4ca8e1b81dccaa3ec54cb8a6
RakibHossen678 <rh491464@gmail.com>
2025-08-01 22:32:10 +0600

    change server url

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://l2-assignment3-beta.vercel.app/api",
+    baseUrl: "https://library-management-server-seven-green.vercel.app/api",
+    // baseUrl: "http://localhost:7000/api",
     credentials: "include",
   }),
   tagTypes: ["books"],
